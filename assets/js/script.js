a98f81d80b5b2f4eb43477e9ad36517df6ed838b
Aldi Nainggolan <88421969+RivaldiYN@users.noreply.github.com>
2022-11-17 23:57:04 +0700

    fix responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,11 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active")
 });
 
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
 for(let i = 0; i < elementClose.length; i++) {
     elementClose[i].addEventListener('click', function() {
         let url = window.location.href.substr(0, window.location.href.indexOf('#'))
